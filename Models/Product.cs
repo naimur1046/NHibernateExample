07e77adae9b2b46ea7e949abc8fea63b75dbadca
naimur rahman rumel <naimurrahman1046@gmail.com>
2024-10-08 13:57:55 +0600

    Add project files.

---
+++ Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHibernateExample.Models
+{
+    public class Product
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Description { get; set; }
+        public virtual int Price {  get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Reflection;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+using NHibernateExample.Models;
+
+namespace NHibernateExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Connection string to the database
+            string connStr = "Data Source=NAIMURRAHMAN;Initial Catalog=STUDENT;TrustServerCertificate=True; Trusted_Connection=True;";
+
+
+            // Configure NHibernate
+            var config = new Configuration();
+            config.DataBaseIntegration(d =>
+            {
+                d.ConnectionString = connStr;
+                d.Dialect<MsSql2012Dialect>();
+                d.Driver<NHibernate.Driver.MicrosoftDataSqlClientDriver>();
+
+            });
+
+            // Add assembly mapping (Product.hbm.xml)
+            config.AddAssembly(Assembly.GetExecutingAssembly());
+
+            // Build session factory
+            var sessionFactory = config.BuildSessionFactory();
+
+            // Open session and begin transaction
+            using (var session = sessionFactory.OpenSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    try
+                    {
+                        // Create a new Product object
+                        var product = new Product
+                        {
+                            Name = "Rumel",
+                            Description = "Hi this is",
+                            Price = 250
+                        };
+
+                        // Save the product to the database
+                        session.Save(product);
+
+                        // Commit the transaction
+                        transaction.Commit();
+                        Console.WriteLine("Product saved successfully.");
+                    }
+                    catch (Exception ex)
+                    {
+                        // Rollback the transaction in case of an error
+                        transaction.Rollback();
+                        Console.WriteLine("Error: " + ex.Message);
+                    }
+                }
+            }
+        }
+    }
+}
